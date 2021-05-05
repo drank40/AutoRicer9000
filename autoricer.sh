75f54f080f81fe04c3b95c67268020bd33a18d7f
drank40 <marzianorenato@gmail.com>
2021-05-05 20:38:54 +0200

    testing

---
+++ autoricer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ "$(whoami)" != root ]]; then
+  echo "This script is meant to be run as root right after install."
+  exit 1
+fi
+pacman -S --noconfirm --quiet cowsay
+INTRO="Welcome to autoricer9000. You should run this script after installing arch and you'll be able to create a user, have sudo, install graphic drivers, xorg, and dwm."
+
+cowsay $INTRO
+echo "What would you like your user to be called?"
+read USERNAME
+useradd -mg wheel $USERNAME
+
